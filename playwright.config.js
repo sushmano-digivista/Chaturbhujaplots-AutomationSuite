9ab3498076a7d197b1654a181ba55d15487e01af
SushMano <sushmano9094@gmail.com>
2026-04-03 11:23:22 +0400

    feat: add Desktop Chrome Slow project with slowMo:800

--- playwright.config.js
+++ playwright.config.js
@@ -22,6 +22,10 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
     {
+      name: 'Desktop Chrome Slow',
+      use: { ...devices['Desktop Chrome'], launchOptions: { slowMo: 800 } },
+    },
+    {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
     },
